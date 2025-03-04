2ffc1f0c9052cc82bfc1e9f84fb21c62ff6118c6
Alfonso Martin Angeles <ambangeles.work@icloud.com>
2025-03-04 21:51:50 +0800

    Add TypeScript types for Product and implement validation schema; update ProductCard and ProductList components to use typed products

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import ProductCard from "./product-card";
+import { Product } from "@/app/types";
 
-const ProductList = ({ data, title, limit }: { data: any; title?: string; limit?: number }) => {
+const ProductList = ({
+	data,
+	title,
+	limit,
+}: {
+	data: Product[];
+	title?: string;
+	limit?: number;
+}) => {
 	const limited_data = limit ? data.slice(0, limit) : data;
 	return (
 		<div className="my-10">
 			<h2 className="h2-bold mb-4">{title}</h2>
 			{data.length ? (
 				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-					{limited_data.map((product: any) => (
+					{limited_data.map((product) => (
 						<ProductCard key={product.slug} product={product} />
 					))}
 				</div>
