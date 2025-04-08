9134350f3f79db0001a9b07fd85eabbf03eea6ac
Alfonso Martin Angeles <ambangeles.work@icloud.com>
2025-04-08 23:23:17 +0800

    Add sign-up page and form with user registration functionality

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import { signIn, signOut } from "@/auth";
-import { signInFormSchema } from "../validators";
+import { prisma } from "@/db/prisma";
+import { hashSync } from "bcrypt-ts-edge";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { signInFormSchema, signUpFormSchema } from "../validators";
+import { formatError } from "@/lib/utils";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
@@ -27,4 +30,44 @@ export async function signInWithCredentials(prevState: unknown, formData: FormDa
 // Sign the user out
 export async function signOutUser() {
 	await signOut();
+}
+
+// Register a new user
+export async function signUp(prevState: unknown, formData: FormData) {
+	try {
+		const user = signUpFormSchema.parse({
+			name: formData.get("name"),
+			email: formData.get("email"),
+			confirmPassword: formData.get("confirmPassword"),
+			password: formData.get("password"),
+		});
+
+		const plainPassword = user.password;
+
+		user.password = hashSync(user.password, 10);
+
+		await prisma.user.create({
+			data: {
+				name: user.name,
+				email: user.email,
+				password: user.password,
+			},
+		});
+
+		await signIn("credentials", {
+			email: user.email,
+			password: plainPassword,
+		});
+
+		return { success: true, message: "User created successfully" };
+	} catch (error) {
+		if (isRedirectError(error)) {
+			throw error;
+		}
+
+		return {
+			success: false,
+			message: formatError(error),
+		};
+	}
 }
