0203d7d52bc1e5dd3796dacca59cd464cf0e4f8a
Alfonso Martin Angeles <ambangeles.work@icloud.com>
2025-05-23 10:46:11 +0800

    Update Arto chat script import URL for improved reliability

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
 				crossOrigin="anonymous"
 				dangerouslySetInnerHTML={{
 					__html: `
-					import Arto from "https://assets.arto.chat/arto.js";
+					import Arto from "https://ucarecdn.com/a384da02-2386-4ff0-a531-60492ac48258/arto.js";
 						Arto.init({
 							uuid: "70825f96-21ae-4221-a6d4-4c7754c961a4"
 						});
