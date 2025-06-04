c1382502292707aca3430d9e0cb7b7f6044261fc
Alfonso Martin Angeles <ambangeles.work@icloud.com>
2025-06-04 09:29:29 +0800

    Update Arto chat script UUID for improved integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
 					__html: `
 					import Arto from "https://assets.arto.chat/arto.js";
 						Arto.init({
-							uuid: "827e7d06-e77d-4eb3-a0b5-30919222c1bb"
+							uuid: "9efee8f5-6089-4279-b6e3-3c2c8a7914f2"
 						});
 				`,
 				}}
