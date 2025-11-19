42eee8901963a0517820f4ce57f84996d2a92006
anonymous <anonymous@localhost>
2025-11-19 09:26:35 +0800

    fix: update Arto widget import URL and change widget type to chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,17 +43,10 @@ export default function RootLayout({
 					crossOrigin="anonymous"
 					dangerouslySetInnerHTML={{
 						__html: `
-							import Arto from "https://assets.arto.chat/arto.js";
+							import Arto from "https://media-staging.arto.chat/arto.js";
 							Arto.init({
-								uuid: "ba04b9b3-0858-4bc1-b7bc-dba041eee498",
-								widgetType: "search",
-								theme: {
-								fonts: {
-									fontRegular: "https://media.arto.chat/fonts/a6f04a1d-74a1-4c81-a584-b85491352fd3/asst_pKp3LPZdsnvR9x8u8UtllYw0/ba04b9b3-0858-4bc1-b7bc-dba041eee498/search/Roboto_Condensed-Regular.ttf",
-									fontMedium: "https://media.arto.chat/fonts/a6f04a1d-74a1-4c81-a584-b85491352fd3/asst_pKp3LPZdsnvR9x8u8UtllYw0/ba04b9b3-0858-4bc1-b7bc-dba041eee498/search/Roboto_Condensed-Medium.ttf",
-									fontBold: "https://media.arto.chat/fonts/a6f04a1d-74a1-4c81-a584-b85491352fd3/asst_pKp3LPZdsnvR9x8u8UtllYw0/ba04b9b3-0858-4bc1-b7bc-dba041eee498/search/Roboto Condensed Bold.ttf"
-								}
-								}
+								uuid: "c74b0ef6-203a-434a-8169-92ecdab13ef7",
+								widgetType: "chat",
 							});
 						`,
 					}}
